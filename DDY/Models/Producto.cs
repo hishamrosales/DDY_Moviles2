89671753fe73f6cdc695e4cd4611784b37ab66ba
Debainilla <debanhiabigailla@gmail.com>
2026-09-02 09:54:35 -0600

    Soporte multiplataforma y nuevos modelos de producto
    Se actualiza DDY.csproj para soportar Android, iOS, MacCatalyst y Windows, se añade BOM y nuevos grupos de elementos para ViewModels y Views. Se agregan las clases ProductoApiDto (DTO para productos de API) y Producto (modelo de producto).

---
+++ DDY/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDY.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public decimal Precio { get; set; }
+
+        public string Categoria { get; set; } = string.Empty;
+
+        public string Imagen { get; set; } = string.Empty;
+
+    }
+}
