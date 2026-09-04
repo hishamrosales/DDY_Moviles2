b29eee7f13a4b333f747de1fc809d3c05641ee4b
hishamrosales <hisham.rosales@gmail.com>
2026-09-03 23:07:15 -0600

    Añadir las clases relacionadas con Detalle. Mover las cartas hardcodeadas a ListaViewModel. Añadir los respectivos Routing y Cambiar momentaneamente las url de las imágenes.

---
+++ DDY/ViewModels/DetalleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DDY.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DDY.ViewModels
+{
+    [QueryProperty(nameof(Cartas), "Carta")]
+    public partial class DetalleViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private CartaPokemon cartas;
+    }
+}
--- DDY/Views/ListaCartas.xaml.cs
+++ DDY/Views/ListaCartas.xaml.cs
@@ -1,64 +1,14 @@
 using DDY.Models;
+using DDY.ViewModels;
 
 namespace DDY.Views;
 
 public partial class ListaCartas : ContentPage
 {
-    List<CartaPokemon> cartas = new List<CartaPokemon>
-        {
-            new CartaPokemon
-            {
-                Nombre = "Pikachu",
-                Categoria = "Pokémon",
-                Tipo = "Eléctrico",
-                Rareza = "Rara",
-                Estado = "Buena",
-                ValorEstimado = 250,
-                Imagen = "pikachu.png",
-                EsFavorito = true
-            },
-
-            new CartaPokemon
-            {
-                Nombre = "Charizard",
-                Categoria = "Pokémon",
-                Tipo = "Fuego",
-                Rareza = "Ultra Rara",
-                Estado = "Nueva",
-                ValorEstimado = 1500,
-                Imagen = "charizard.png",
-                EsFavorito = false
-            },
-
-            new CartaPokemon
-            {
-                Nombre = "Mew",
-                Categoria = "Pokémon",
-                Tipo = "Psíquico",
-                Rareza = "Rara",
-                Estado = "Buena",
-                ValorEstimado = 800,
-                Imagen = "mew.png",
-                EsFavorito = true
-            },
-
-            new CartaPokemon
-            {
-                Nombre = "Bulbasaur",
-                Categoria = "Pokémon",
-                Tipo = "Planta",
-                Rareza = "Común",
-                Estado = "Buena",
-                ValorEstimado = 100,
-                Imagen = "bulbasaur.png",
-                EsFavorito = false
-            }
-        };
-
-    public ListaCartas()
+    public ListaCartas(ListaViewModel vm)
     {
         InitializeComponent();
 
-        BindingContext = cartas;
+        BindingContext = vm;
     }
 }
